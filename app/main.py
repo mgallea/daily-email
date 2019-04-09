ad99a7dca45c741af641dee228133a327a1ef832
Matthew Gallea <mattgallea@gmail.com>
2019-04-08 21:04:40 -0400

    Add weather functionality to Newsletter

--- app/main.py
+++ app/main.py
@@ -31,7 +31,7 @@ date = str(year) + "-" + str(month) + "-" + str(day)
 #Get Top Headlines
 wsj_top_headlines = newsapi.get_top_headlines(sources='the-wall-street-journal')
 nyt_top_headlines = newsapi.get_top_headlines(sources='the-new-york-times')
-donald_top_headlines = newsapi.get_top_headlines(q='Donald Trump')
+donald_top_headlines = newsapi.get_top_headlines(q='Donald Trump',language='en')
 
 wTitle = FormatTopHeadlines(wsj_top_headlines,"title")
 wBody = FormatTopHeadlines(wsj_top_headlines,"description")
@@ -49,9 +49,8 @@ fullWeather = requests.get('https://api.openweathermap.org/data/2.5/forecast?zip
 
 #Parse Weather Data
 temp = TemperatureParser(fullWeather)
-print(str(temp))
-
-exit()
+dailyHigh = str(temp[0])
+dailyLow = str(temp[1])
 
 #Prepare the email
 message = Mail(
@@ -60,53 +59,55 @@ message = Mail(
     to_emails = MY_EMAIL_ADDRESS
     )
 message.dynamic_template_data ={
+    'daily-high' : dailyHigh,
+    'daily-low' : dailyLow,
     'source1':"THE WALL STREET JOURNAL",
   	'l1-1': wURL[0],
- 	'h1-1': wTitle[0],
- 	'b1-1': wBody[0],
+ 	  'h1-1': wTitle[0],
+ 	  'b1-1': wBody[0],
   	'l1-2': wURL[1],
   	'h1-2': wTitle[1],
   	'b1-2': wBody[1],
   	'l1-3': wURL[2],
   	'h1-3': wTitle[2],
- 	'b1-3': wBody[2],
+ 	  'b1-3': wBody[2],
   	'l1-4': wURL[3],
   	'h1-4': wTitle[3],
   	'b1-4': wBody[3],
   	'l1-5': wURL[4],
- 	'h1-5': wTitle[4],
+  	'h1-5': wTitle[4],
   	'b1-5': wBody[4],
   	'source2':"THE NEW YORK TIMES",
   	'l2-1': nURL[0],
- 	'h2-1': nTitle[0],
- 	'b2-1': nBody[0],
+ 	  'h2-1': nTitle[0],
+ 	  'b2-1': nBody[0],
   	'l2-2': nURL[1],
   	'h2-2': nTitle[1],
   	'b2-2': nBody[1],
   	'l2-3': nURL[2],
   	'h2-3': nTitle[2],
- 	'b2-3': nBody[2],
+  	'b2-3': nBody[2],
   	'l2-4': nURL[3],
   	'h2-4': nTitle[3],
   	'b2-4': nBody[3],
   	'l2-5': nURL[4],
- 	'h2-5': nTitle[4],
+   	'h2-5': nTitle[4],
   	'b2-5': nBody[4],
-  	'source3':'WHAT DID TRUMP DO THIS TIME?',
+  	'source3':'NEWS ABOUT THE PRESIDENT',
   	'l3-1': dURL[0],
- 	'h3-1': dTitle[0],
- 	'b3-1': dBody[0],
+   	'h3-1': dTitle[0],
+   	'b3-1': dBody[0],
   	'l3-2': dURL[1],
   	'h3-2': dTitle[1],
   	'b3-2': dBody[1],
   	'l3-3': dURL[2],
   	'h3-3': dTitle[2],
- 	'b3-3': dBody[2],
+  	'b3-3': dBody[2],
   	'l3-4': dURL[3],
   	'h3-4': dTitle[3],
   	'b3-4': dBody[3],
   	'l3-5': dURL[4],
- 	'h3-5': dTitle[4],
+  	'h3-5': dTitle[4],
   	'b3-5': dBody[4],
     }
 message.template_id = SENDGRID_TEMPLATE_ID
