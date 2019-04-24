1547f5c70032133a0ae2739c07cf71e817cc3cc9
Matthew Gallea <mattgallea@gmail.com>
2019-04-24 16:29:17 -0400

    Update Functions File

--- test/test_email.py
+++ test/test_email.py
@@ -1,5 +1,23 @@
-from app.functions import *
+from app.functions_file import *
+from app.main import (
+	SENDGRID_API_KEY,
+	MY_EMAIL_ADDRESS,
+	NEWS_API_KEY,
+	SENDGRID_TEMPLATE_ID,
+	OPENWEATHER_API_KEY
+	)
+from newsapi.newsapi_client import NewsApiClient
+
+CI_ENV = os.environ.get("CI", "OOPS") == "true" # expect default environment variable setting of "CI=true" on Travis CI, see: https://docs.travis-ci.com/user/environment-variables/#default-environment-variables
+SKIP_REASON = "to avoid issuing requests from the CI server"
+	
 
 def test_Kelvin():
 	k = KelvinToFahrenheit(298)
 	assert k == 77
+
+@pytest.mark.skipif(CI_ENV==True, reason=SKIP_REASON)
+def test_Get_Top_Headlines():
+	newsapi = NewsApiClient(api_key=NEWS_API_KEY)
+	wsj_top_headlines = newsapi.get_top_headlines(sources='the-wall-street-journal')
+	assert len(wsj_top_headlines) == 3
